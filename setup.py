9046bf3d2b864de224df5152a1fc7f34c1fdef30
Maxim Kashevsky <maxim.kashevsky@scorum.com>
2018-10-23 12:13:38 +0300

    changed version to 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ required = ['ecdsa', 'pytest', 'pycrypto', 'scrypt', 'aiohttp', 'requests']
 
 setup(
     name='scorum',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(exclude=["tests"]),
     long_description=open('README.md').read(),
     install_requires=required
